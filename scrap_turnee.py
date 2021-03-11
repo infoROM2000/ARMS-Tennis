5da61fa13cff098d9fa75dc4fcddb66d0890af34
Olteanu Dragos <54898936+infoROM2000@users.noreply.github.com>
2021-03-11 17:21:52 +0200

    scrap meciuri

--- scrap_turnee.py
+++ scrap_turnee.py
@@ -1,17 +1,34 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup as soup
 
-linkuri = []
+jucatori = []
 f = open("turnee.txt", "r")
-options = Options()
-options.add_argument("--window-size=600,400")
 url_uri = f.readlines()
 url_uri.pop(0)  # primul element din lista e \n
-
-browser = webdriver.Chrome(options=options)
-browser.get(url_uri[2])
-rezultate = browser.find_elements_by_tag_name('tr')
-for meci in rezultate:
-    a = meci.get_attribute('outerHTML')
-    print(a)
-browser.quit()
+f.close()
+f = open("meciuri.txt", "w")
+f.close()
+f = open("meciuri.txt", "a")
+i = 0
+for url in url_uri:
+    req = Request("https://www.atptour.com" + url, headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(req).read()
+    sursa = soup(webpage, "html.parser")
+    meciuri = sursa.find_all("tr")
+    for e in meciuri:
+        jocuri = e.find_all("td", class_="day-table-name")
+        if jocuri:
+            for joc in jocuri:
+                juc = joc.find_all("a")
+                if juc:
+                    jucator = juc[0]['href']
+                    jucatori.append(str(jucator))
+                else:
+                    jucatori.append("Bye")
+    jucatori.append('\n')
+    print(i)
+    i+=1
+n=i
+for jucator in jucatori:
+    f.write('\n' + jucator)
+f.close()
